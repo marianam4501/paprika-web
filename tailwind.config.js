10361a5be5e25c877091b47edc733b54094cd085
Mariana Murillo <mariana@primarix.com>
2022-06-15 19:01:58 -0600

    add paprika colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
       screens: {
         
       },
+      colors: {
+        'white': '#ffffff',
+        'black': '#000000',
+        'light-orange': '#ff7a00',
+        'dark-orange': '#D44000',
+        'brown': '#864000'
+      }
     },
   },
   plugins: [],
