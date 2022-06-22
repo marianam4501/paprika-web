2253d7142509574f0e425d5917c64ad7632ac338
Nathan.MIranda <nathan.miranda@ucr.ac.cr>
2022-06-21 21:25:36 -0600

    Welcome page implemented, added images fil, contains the images of the background

--- src/Components/Footer/Index.js
+++ src/Components/Footer/Index.js
@@ -1,46 +1,52 @@
 import React from "react";
 
 function Footer() {
-	return (
-		<>
-			<div className="bg-black h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-5">
-				<div >
-					<ul>
-						<img
-                            className="object-cover relative h-20"
-                            src="https://rb.gy/iq70yv"
-                            alt="Logo Paprika"
-                      />
-					</ul>
-				</div>
-				<div className="p-5">
-					<ul>
-						<p className="text-dark-orange text-2xl pb-4  hover:text-white cursor-pointer">About</p>
-						
-					</ul>
-				</div>
-				<div className="p-5">
-					<ul>
-						<p className="text-dark-orange text-2xl pb-4 hover:text-white cursor-pointer">Contact Us</p>
-					</ul>
-				</div>
-				<div className="p-5">
-					<ul>
-						<p className="text-dark-orange text-2xl pb-4 hover:text-white cursor-pointer">Support</p>
-					</ul>
-				</div>
-			</div>
-			<div className="flex flex-col justify-center items-center text-center  p-2 bg-black">
-				<h1 className=" text-white">
-					© 2022 All rights reserved | Build with ❤ by{" "}
-					<span className="hover:text-light-orange font-semibold cursor-pointer">
-						Paprika{" "}
-					</span>
-                    team
-				</h1>
-			</div>
-		</>
-	);
+  return (
+    <>
+	
+      <div className="bg-black h-full w-full flex md:flex-row flex-col justify-around items-start p-2">
+        <div>
+          <ul>
+            <img
+              className="object-cover relative h-16 pr-28"
+              src="https://rb.gy/iq70yv"
+              alt="Logo Paprika"
+            />
+          </ul>
+        </div>
+        <div className="p-3">
+          <ul>
+            <p className="text-dark-orange text-xl pb-4  hover:text-white cursor-pointer">
+              About
+            </p>
+          </ul>
+        </div>
+        <div className="p-3">
+          <ul>
+            <p className="text-dark-orange text-xl pb-4 hover:text-white cursor-pointer">
+              Contact Us
+            </p>
+          </ul>
+        </div>
+        <div className="p-3">
+          <ul>
+            <p className="text-dark-orange text-xl pb-4 hover:text-white cursor-pointer">
+              Support
+            </p>
+          </ul>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center text-center  p-1 bg-black">
+        <h1 className=" text-white">
+          © 2022 All rights reserved | Build with ❤ by{" "}
+          <span className="hover:text-light-orange font-semibold cursor-pointer">
+            Paprika{" "}
+          </span>
+          team
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,20 +1,109 @@
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import React from "react";
 import SimpleHeader from "../../Components/SimpleHeader/Index";
 import Footer from "../../Components/Footer/Index";
 export function Home() {
   return (
     <div className="p-0">
-      <div className="pb-5">
+      <div className="pb-0">
         <SimpleHeader />
       </div>
-      <div className="h-80">
-        <body>
-          <p>Hello world!</p>
-        </body>
-      </div>
-      <div className="pt-5">
-        <Footer />
-      </div>
+
+      {/* Images background */}
+      
+      <main className="static h-full">
+        
+        <div className="h-full w-full pt-44 fixed space-y-20 z-30">
+          <div className="bg-white h-56 w-full gap-0 grid justify-items-center">
+            <h1 className="text-lg font-semibold tracking-wide text-center pt-5 px-10">
+              Join our cooking comunity
+            </h1>
+            <button
+              className="bg-light-orange hover:bg-dark-orange text-black 
+            hover:text-white font-semibold  mx-4 h-10 w-auto px-5 rounded "
+            >
+              <a href="/register">Join Paprika</a>
+              
+            </button>
+            <h1 className="text-lg font-semibold tracking-wide text-center px-10">
+              or
+            </h1>
+            <button
+              className="bg-light-orange hover:bg-dark-orange text-black 
+            hover:text-white font-semibold  mx-4 h-10 w-auto px-11 rounded"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+
+        
+
+        <div className="mx-5 columns-2 -z-0">
+          <div className="px-1 flex flex-col space-y-5">
+            {/*   IMG 1  */}
+            <img
+              className="object-cover relative h-auto w-auto rounded-lg"
+              alt="test"
+              src="https://i.ibb.co/B2Sv2HQ/f-5.jpg"
+            />
+            {/*   IMG 2  (prime)*/}
+            <img
+              className="object-cover relativeh-auto w-auto rounded-lg"
+              alt="test"
+              src="https://i.ibb.co/VYgGCxk/f1.jpg"
+            />
+            {/*   IMG 3  */}
+            <img
+              className="object-cover relative h-auto w-auto rounded-lg"
+              alt="test"
+              src="https://i.ibb.co/pxdv1PF/f-3.jpg"
+            />
+            {/*   IMG 4  */}
+            <img
+              className="object-cover relative h-auto w-auto rounded-lg"
+              alt="test"
+              src="https://i.ibb.co/vxBRGfn/f-7.jpg"
+            />
+            {/*   IMG 5  (prime)*/}
+            <img
+              className="object-cover relativeh-auto w-auto rounded-lg"
+              alt="test"
+              src="https://i.ibb.co/Zm4Hx0p/f-4.jpg"
+            />
+            {/*   IMG 6  */}
+            <img
+              className="object-cover relative h-auto w-auto rounded-lg"
+              alt="test"
+              src="https://bit.ly/3n9MbQp"
+            />
+             {/*   IMG 7  */}
+             <img
+              className="object-cover relative h-auto w-auto rounded-lg"
+              alt="test"
+              src="https://i.ibb.co/MNm723z/f-6.jpg"
+            />
+            {/*   IMG 8  (prime)*/}
+            <img
+              className="object-cover relative h-auto w-auto rounded-lg"
+              alt="test"
+              src="https://i.ibb.co/pRqfVT0/f-8.jpg"
+            />
+            {/*   IMG 9  */}
+            <img
+              className="object-cover relative h-auto w-auto rounded-lg"
+              alt="test"
+              src="https://i.ibb.co/BZpw6Vj/f-9.jpg"
+            />
+          </div>
+          {/*-----------------------------------------------------------------------------------*/}
+          
+        </div>
+        
+      </main>
+      <div className="relative inset-x-0 bottom-0 z-40">
+          <Footer />
+        </div>
     </div>
   );
 }
