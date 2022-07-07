602b8e4386aa23c011303a61b9f101b975544c27
Nathan.MIranda <nathan.miranda@ucr.ac.cr>
2022-07-06 22:50:22 -0600

    fixes in recepies and home

--- src/Pages/AddRecipe/index.js
+++ src/Pages/AddRecipe/index.js
@@ -7,30 +7,35 @@ import { v4 as uuidv4 } from "uuid";
 
 import { PlusIcon } from "@heroicons/react/outline";
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 export function AddRecipe() {
-  const [ingredients, setIngredients] = useState([])
+  const [ingredients, setIngredients] = useState([]);
+
+  const [recepiPicture, setRecepiPicture] = useState(null);
 
   function handleAddIngredient(e) {
-    
-    setIngredients(prevIngredients => {
-      return [...prevIngredients, {id: uuidv4()}] // use id?
-    })
+    setIngredients((prevIngredients) => {
+      return [...prevIngredients, { id: uuidv4() }]; // use id?
+    });
   }
 
   useEffect(() => {
-    const storedIngredients = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedIngredients) setIngredients(storedIngredients)
-  }, [])
+    const storedIngredients = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    if (storedIngredients) setIngredients(storedIngredients);
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(ingredients))
-  }, [ingredients])
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(ingredients));
+  }, [ingredients]);
 
   function handleDeleteIngredient(id) {
-    const newIngredients = ingredients.filter(ingredient => ingredient.id !== id)
-    setIngredients(newIngredients)
+    const newIngredients = ingredients.filter(
+      (ingredient) => ingredient.id !== id
+    );
+    setIngredients(newIngredients);
   }
 
   return (
@@ -54,42 +59,95 @@ export function AddRecipe() {
           ></input>
           {/* IMAGE INPUT */}
           <label className="block text-lg  text-black">Meal Picture:</label>
-          <div className="content-center mt-1 flex justify-center px-6 pt-5 pb-6 border-2 h-[200px] border-gray-300 border-dashed rounded-md">
-            <div className="space-y-1 text-center justify-center mt-5">
-              <img
-                className="object-cover h-14 w-22 justify-self-center ml-20"
-                src="https://i.ibb.co/dBvR1v0/fast-food.png"
-                alt="Logo Paprika"
-              />
-              <div className="flex text-sm text-gray-600">
-                <label
-                  htmlFor="file-upload"
-                  className="relative cursor-pointer bg-white rounded-md font-medium text-dark-orange hover:text-light-orange focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-light-orange"
-                >
-                  <span>Upload a Picture</span>
-                  <input
-                    id="file-upload"
-                    name="file-upload"
-                    type="file"
-                    className="sr-only"
+
+          <div className="content-center mt-1 flex justify-center px-6 pt-5 pb-6 border-2 h-auto border-gray-300 border-dashed rounded-md">
+            {!recepiPicture && (
+              <div className=" space-y-1 text-center justify-center mt-5">
+                <div className="columns-1 justify-center  text-sm text-gray-600">
+                  <img
+                    className="object-cover h-14 w-22 mb-3 mx-[92px]"
+                    src="https://i.ibb.co/dBvR1v0/fast-food.png"
+                    alt="Logo Paprika"
                   />
-                </label>
-                <p className="pl-1">or drag and drop</p>
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer bg-white rounded-md font-medium text-dark-orange hover:text-light-orange focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-light-orange"
+                  >
+                    {/* <span>Upload a Picture</span> */}
+                    <input
+                      id="recepiPicture"
+                      onChange={(evt) => {
+                        setRecepiPicture(evt.target.files[0]);
+                      }}
+                      name="file-upload"
+                      type="file"
+                      className="w-[185px]"
+                    />
+                  </label>
+                </div>
               </div>
-              <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+            )}
+
+            {recepiPicture && (
+              <div>
+                <img
+                  src={URL.createObjectURL(recepiPicture)}
+                  alt="Product preview"
+                  className="rounded-xl"
+                />
+
+                <button
+                  onClick={() => {
+                    setRecepiPicture(null);
+                  }}
+                  className="bg-red-600 text-white px-4 py-2 rounded-md"
+                >
+                  Eliminar
+                </button>
+              </div>
+            )}
+          </div>
+
+          {/* <label htmlFor="productPhoto">Foto del producto</label>
+            <input
+              className="block w-full mb-4 border rounded-md"
+              id="productPhoto"
+              onChange={(evt) => {
+                setProductPicture(evt.target.files[0]);
+              }}
+              type="file"
+            />
+            {productPicture && (
+              <img
+                src={URL.createObjectURL(productPicture)}
+                alt="Product preview"
+              />
+            )} */}
+
+          {/* {foto && (
+            <div>
+              <button
+                onClick={() => {
+                  setFoto(null);
+                }}
+                className="bg-red-600 text-white px-4 py-2 rounded-md"
+              >
+                Eliminar
+              </button>
+              <img
+                className="max-h-56 mt-8"
+                src={URL.createObjectURL(foto)}
+                alt="Foto del nuevo artículo"
+              />
             </div>
-          </div>
-          <div className="py-3 bg-gray-50 sm:px-6">
-            <button
-              type="submit"
-              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-black bg-light-orange hover:bg-dark-orange hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
-            >
-              Upload
-            </button>
-          </div>
+          )} */}
+          {/*  INGREDIENTS */}
           <label className="block text-lg  text-black">Ingredients:</label>
-          
-          <IngredientList ingredients={ingredients} handleDeleteIngredient={handleDeleteIngredient}/>
+
+          <IngredientList
+            ingredients={ingredients}
+            handleDeleteIngredient={handleDeleteIngredient}
+          />
           <button
             type="submit"
             className="inline-flex justify-center my-3 px-4 py-3 w-full border border-transparent shadow-sm text-lg font-medium rounded-md text-white bg-black hover:bg-dark-orange hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
@@ -114,7 +172,8 @@ export function AddRecipe() {
             type="submit"
             className="inline-flex justify-center mt-10 px-4 py-3 w-full border border-transparent shadow-sm text-lg font-medium rounded-md text-black bg-light-orange hover:bg-dark-orange hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
           >
-            Post your recipe! 🍜
+            <a href="Add_recipe">Post your recipe! 🍜</a>
+            
           </button>
         </div>
       </main>
