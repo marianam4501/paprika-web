1e5281eebff66b1c7344a9249aa4716c1eb41c40
Cesar-L21 <71990742+Cesar-Lop32@users.noreply.github.com>
2022-07-09 03:00:07 -0600

    merge with interface branch

--- src/Components/FeedPosts/Post.js
+++ src/Components/FeedPosts/Post.js
@@ -1,39 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux/es/exports";
+import { getRecipe } from "../../Slices/Recipes/Requests/getRecipeById";
 // import { HeartIcon } from "@heroicons/react/solid";
 // import { useState } from "react";
 
-export default function RecepiesColumn({ recepi }) {
-  // const [likes, setLikes] = useState(100);
-  // const [liked, setLiked] = useState(false);
-
-  // function handleLike(){
-  //   setLiked(!liked)
-  //   handleNumLike()
-  // }
-
-  // function handleNumLike() {
-    
-  //   if(!liked){
-  //     setLikes(likes + 1)
-  //   }else{
-  //     setLikes(likes - 1)
-  //   }
-    
-  // }
-
+export default function RecepiesColumn({ post }) {
+  const dispatch = useDispatch();
   return (
-    <div id={recepi.id} key={`product_${recepi.id}`} className="px-1 flex flex-col space-y-5">
+    <div id={post.id} key={`product_${post.id}`} className="px-1 flex flex-col space-y-5">
           <Link to="/recipe">
             <img
               className="object-cover relative h-60 w-44 rounded-lg m-0 -z-0"
-              src={recepi.image}
-              alt={recepi.name}
+              src={post.image}
+              alt={post.name}
+              onClick={() => {
+                dispatch(getRecipe(post.id));
+               }}
             />
           </Link>
           {/* <div className="columns-2"> */}
             
-            <label className="text-black text-sm">{recepi.name}</label>
+            <label className="text-black text-sm">{post.name}</label>
             </div>
            
   );
--- src/Components/FeedPosts/PostsList.js
+++ src/Components/FeedPosts/PostsList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Post from "./Post";
 
-export default function PostsList({ Posts }) {
+export default function PostsList({ feed }) {
   return (
     <div className="mx-5 columns-2 -z-0">
       <div className="px-1 flex flex-col space-y-5 my-5">
-      {Posts.map((post) => {
+      {feed.map((post) => {
         console.log(post.id);
-        return <Post key={post.id} recepi={post} />;
+        return <Post key={post.id} post={post} />;
       })}
     </div>
     </div>
