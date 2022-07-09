9c06c3e5bcd72257bd0ef3b20353e98adae184b0
Cesar-L21 <71990742+Cesar-Lop32@users.noreply.github.com>
2022-07-08 23:31:59 -0600

    erase logOut

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -6,7 +6,6 @@ import Register from "../Register";
 import {Profile} from "../Profile";
 import ViewRecipe from "../Recipe/ViewRecipe";
 import Feed from "../Feed/Feed";
-import  LogOut  from "../Login/LogOut";
 
 export default function App() {
   //const user = useSelector(selectUser);
@@ -20,7 +19,6 @@ export default function App() {
       <Route path="Feed" element = { <Feed/>}/>
       <Route path="Add_recipe" element={ <AddRecipe />} />
       <Route path="Profile" element={ <Profile />} />
-      <Route path="logOut" element={ <LogOut />} />
     </Routes>
   )
 }
