15d5d56cb2da183e2de33988da0bdcf8954b1a07
Mariana Murillo <mariana@primarix.com>
2022-07-10 17:12:25 -0600

    add authorization

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -6,6 +6,9 @@ import Register from "../Register";
 import {Profile} from "../Profile";
 import ViewRecipe from "../Recipe/ViewRecipe";
 import Feed from "../Feed/Feed";
+import Unathorized from "../Unathorized"
+import PrivateRoute from "../../Components/PrivateRoute";
+import { ROLES } from "../../utils/constants";
 // import { useSelector } from "react-redux";
 // import Spinner from "../../Components/Loading/index";
 
@@ -19,10 +22,13 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="login" element={ <Login />} />
       <Route path="register" element={ <Register />} />
-      <Route path="recipe/:id" element = { <ViewRecipe/>}/>
-      <Route path="Feed" element = { <Feed/>}/>
-      <Route path="Add_recipe" element={ <AddRecipe />} />
-      <Route path="Profile" element={ <Profile />} />
+      <Route path="/no-autorizado" element={<Unathorized />} />
+      <Route element={<PrivateRoute allowedRoles={[ROLES.ADMIN]} />}>
+        <Route path="recipe/:id" element = { <ViewRecipe/>}/>
+        <Route path="Feed" element = { <Feed/>}/>
+        <Route path="Add_recipe" element={ <AddRecipe />} />
+        <Route path="Profile" element={ <Profile />} />
+      </Route>
     </Routes>
   ) ; 
   
