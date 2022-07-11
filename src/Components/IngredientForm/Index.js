a9a0d4970322b9d29d2b267631a9729b68b4a5e6
Nathan.MIranda <nathan.miranda@ucr.ac.cr>
2022-07-10 20:34:34 -0600

    fix x2

--- src/Components/IngredientForm/Index.js
+++ src/Components/IngredientForm/Index.js
@@ -10,7 +10,7 @@ export default function IngedientForm({
     id: uuidv4(),
     name: "",
     quantity: "",
-    measure: "",
+    measurement: "",
   });
   
   
--- src/Components/RecepiIngredients/Ingredient.js
+++ src/Components/RecepiIngredients/Ingredient.js
@@ -5,7 +5,7 @@ export default function Ingredient({ ingredient }) {
     <tr className="border-y-white border">
       <th className="text-base font-normal">{ingredient.name} </th>
       <th className="text-base font-normal"> {ingredient.quantity}</th>
-      <th className="text-base font-normal"> {ingredient.measure}</th>
+      <th className="text-base font-normal"> {ingredient.measurement}</th>
     </tr>
   );
 }
